d6d7e2700026d8aef5d45be0a1dc46d0dc23f86d
Luca Spalierno <spalierno@gmail.com>
2020-10-21 22:03:06 +0200

    Formatting.

--- networking/lib/main.dart
+++ networking/lib/main.dart
@@ -51,164 +51,164 @@ class _SendInformationWidgetState extends State<SendInformationWidget> {
         horizontal: 20.0,
         vertical: 40,
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Card(
-            elevation: 20,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            margin: EdgeInsets.all(10),
-            color: Colors.lightBlue,
-            child: ListTile(
-              contentPadding: EdgeInsets.all(10),
-              leading: Image.asset(
-                'assets/logo.png',
-                height: 60,
-              ),
-              subtitle: TextFormField(
-                decoration: InputDecoration(
-                    labelStyle: TextStyle(color: Colors.amberAccent),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.amberAccent, width: 2),
-                    ),
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                    labelText: 'Enter your name',
-                    hintText: 'No name set'),
-                onChanged: (newValue) {
-                  setState(() {
-                    _name = newValue;
-                  });
-                },
-              ),
-            ),
-          ),
-          Card(
-            elevation: 20,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            margin: EdgeInsets.all(10),
-            color: Colors.lightBlue,
-            child: ListTile(
-              contentPadding: EdgeInsets.all(10),
-              leading: Image.asset(
-                'assets/logo.png',
-                height: 60,
-              ),
-              title: Text('Greetings'),
-              subtitle: Text(_greeting ?? 'No greeting :('),
-            ),
-          ),
-          Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Card(
               elevation: 20,
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
               margin: EdgeInsets.all(10),
               color: Colors.lightBlue,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 20,
+              child: ListTile(
+                contentPadding: EdgeInsets.all(10),
+                leading: Image.asset(
+                  'assets/logo.png',
+                  height: 60,
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelStyle: TextStyle(color: Colors.amberAccent),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.amberAccent,
-                            width: 2,
-                          ),
-                        ),
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                        labelText: 'Enter username',
-                        hintText: 'No username set',
+                subtitle: TextFormField(
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Colors.amberAccent),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.amberAccent, width: 2),
                       ),
-                      onChanged: (newValue) {
-                        setState(() {
-                          _firstField = newValue.isNotEmpty ? newValue : null;
-                        });
-                      },
-                    ),
-                    Container(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelStyle: TextStyle(color: Colors.amberAccent),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.amberAccent,
-                            width: 2,
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      labelText: 'Enter your name',
+                      hintText: 'No name set'),
+                  onChanged: (newValue) {
+                    setState(() {
+                      _name = newValue;
+                    });
+                  },
+                ),
+              ),
+            ),
+            Card(
+              elevation: 20,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              margin: EdgeInsets.all(10),
+              color: Colors.lightBlue,
+              child: ListTile(
+                contentPadding: EdgeInsets.all(10),
+                leading: Image.asset(
+                  'assets/logo.png',
+                  height: 60,
+                ),
+                title: Text('Greetings'),
+                subtitle: Text(_greeting ?? 'No greeting :('),
+              ),
+            ),
+            Card(
+                elevation: 20,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                margin: EdgeInsets.all(10),
+                color: Colors.lightBlue,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 20,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelStyle: TextStyle(color: Colors.amberAccent),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.amberAccent,
+                              width: 2,
+                            ),
                           ),
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          labelText: 'Enter username',
+                          hintText: 'No username set',
                         ),
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                        labelText: 'Enter content',
-                        hintText: 'No content',
+                        onChanged: (newValue) {
+                          setState(() {
+                            _firstField = newValue.isNotEmpty ? newValue : null;
+                          });
+                        },
                       ),
-                      onChanged: (newValue) {
-                        setState(() {
-                          _secondField = newValue.isNotEmpty ? newValue : null;
-                        });
-                      },
-                    )
-                  ],
+                      Container(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelStyle: TextStyle(color: Colors.amberAccent),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.amberAccent,
+                              width: 2,
+                            ),
+                          ),
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          labelText: 'Enter content',
+                          hintText: 'No content',
+                        ),
+                        onChanged: (newValue) {
+                          setState(() {
+                            _secondField = newValue.isNotEmpty ? newValue : null;
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                RaisedButton(
+                  elevation: 20,
+                  color: Colors.lightBlue[100],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  onPressed: () async {
+                    final result = await _httpService.getHelloFromBackend(name: _name);
+                    setState(() {
+                      _greeting = result + '  🎉';
+                    });
+                  },
+                  child: Text('Greet'),
                 ),
-              )),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              RaisedButton(
-                elevation: 20,
-                color: Colors.lightBlue[100],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+                RaisedButton(
+                  elevation: 20,
+                  color: Colors.lightBlue[100],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _greeting = null;
+                    });
+                  },
+                  child: Text('Reset'),
                 ),
-                onPressed: () async {
-                  final result = await _httpService.getHelloFromBackend(name: _name);
-                  setState(() {
-                    _greeting = result + '  🎉';
-                  });
-                },
-                child: Text('Greet'),
-              ),
-              RaisedButton(
-                elevation: 20,
-                color: Colors.lightBlue[100],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
+                RaisedButton(
+                  elevation: 20,
+                  disabledColor: Colors.grey[300],
+                  disabledElevation: 0,
+                  color: Colors.lightBlue[100],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  onPressed: (_firstField != null && _secondField != null)
+                      ? () async {
+                          await _httpService.sendNewContent(_firstField, _secondField);
+                        }
+                      : null,
+                  child: Text('Send data'),
                 ),
-                onPressed: () {
-                  setState(() {
-                    _greeting = null;
-                  });
-                },
-                child: Text('Reset'),
-              ),
-              RaisedButton(
-                elevation: 20,
-                disabledColor: Colors.grey[300],
-                disabledElevation: 0,
-                color: Colors.lightBlue[100],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                onPressed: (_firstField != null && _secondField != null)
-                    ? () async {
-                        await _httpService.sendNewContent(_firstField, _secondField);
-                      }
-                    : null,
-                child: Text('Send data'),
-              ),
-            ],
-          )
-        ],
+              ],
+            )
+          ],
       ),
     );
   }
 
   @override
   void initState() {
-    _httpService = HttpService(baseUri: '<expose with ngrok http 5000>');
+    _httpService = HttpService(baseUri: '<expose with: ngrok http 5000>');
     super.initState();
   }
 }
